38df7a8264422bce93eebb9fc40dd7eb2b54b308
Pham Viet Hung <viethungpham628@gmail.com>
2023-04-09 07:35:25 +0700

    bai dong ho

--- May tinh/script.js
+++ May tinh/script.js
@@ -2,12 +2,12 @@ function clearScreen() {
     document.getElementById("result").value = "";
 }
 
-// This function display values
+
 function display(value) {
     document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and returns result
+
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
